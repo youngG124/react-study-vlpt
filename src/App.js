b77d44b2730b03e0f1e0877e7670d97eb9023e4c
youngG124 <newyoung124@naver.com>
2022-02-02 18:05:02 +0900

    JSX

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import Hello from './Hello';
+import './App.css';
 
 function App() {
+
+  const name = 'react';
+  const style = {
+    backgroundColor : 'black',
+    color : 'aqua',
+    fontSize : 24,
+    padding : '1rem'
+  }
+
   return (
-    <div className="App">
+    <>
       <Hello />
-    </div>
+      <div style={style}>{name}</div>
+      <div className = "gray-box"></div>
+    </>
   );
 }
 
