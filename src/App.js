2a2983da31cb749291aedcbe3a3faf8359abb3d0
youngG124 <newyoung124@naver.com>
2022-02-03 13:12:09 +0900

    props, setState 사용

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
 import React from 'react';
-import Hello from './Hello';
-import './App.css';
+import InputSample from './InputSample';
 
 function App() {
 
-  const name = 'react';
-  const style = {
-    backgroundColor : 'black',
-    color : 'aqua',
-    fontSize : 24,
-    padding : '1rem'
-  }
-
   return (
-    <>
-      <Hello />
-      <div style={style}>{name}</div>
-      <div className = "gray-box"></div>
-    </>
+    <InputSample/>
   );
 }
 
