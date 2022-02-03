88c4ae6c04b9712668f0105aad1dd99eedfbb90d
youngG124 <newyoung124@naver.com>
2022-02-03 13:23:06 +0900

    input State

--- src/InputSample.js
+++ src/InputSample.js
@@ -1,23 +1,38 @@
 import React, { useState } from 'react';
 
 function InputSample() {
-    const [ text, setText ] = useState('');
+    const [inputs, setInputs] = useState({
+        name : '',
+        nickname : ''
+    });
+
+    const { name, nickname } = inputs;
 
     const onChange = (e) => {
-        setText(e.target.value);
+        const { value, name } = e.target;
+        setInputs({
+            ...inputs,
+            [name] : value
+        });
+
     };
 
     const onReset = () => {
-        setText('');
+        setInputs({
+            nam:'',
+            nickname:''
+        })
     };
 
     return (
         <div>
-        <input onChange={onChange} value={text}/>
-        <button onClick={onReset}>초기화</button>
-        <div>
-            <b>값: {text}</b>
-        </div>
+            <input placeholder="이름" onChange={onChange} value={name}/>
+            <input placeholder="닉네임" onChange={onChange} value={nickname}/>
+            <button onClick={onReset}>초기화</button>
+            <div>
+                <b>값: </b>
+                {name} ({nickname})
+            </div>
         </div>
     );
 }
